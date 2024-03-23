791a23df386841e104e146929fc2848433c21348
DavidLegarre <davidls920@gmail.com>
2024-03-23 20:10:43 +0100

    Initial Window

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,39 @@
+mod arena;
+mod snake;
+
 use bevy::{ prelude::* };
 
-#[derive(Component)]
-struct SnakeHead;
-
-const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
-
-fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
-}
-
-fn spawn_snake(mut commands: Commands) {
-    commands.spawn((
-        SnakeHead,
-        SpriteBundle {
-            sprite: Sprite {
-                color: SNAKE_HEAD_COLOR,
-                ..default()
-            },
-            transform: Transform {
-                scale: Vec3::new(10.0, 10.0, 10.0),
-                ..default()
-            },
-            ..default()
-        },
-    ));
-}
-
-fn snake_movement(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut head_positions: Query<(&SnakeHead, &mut Transform)>
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>
 ) {
-    const SPEED: f32 = 2.0;
-    let mut direction = Vec3::ZERO;
-    for (_head, mut transform) in head_positions.iter_mut() {
-        if keyboard_input.pressed(KeyCode::ArrowLeft) {
-            direction.x -= SPEED;
-        }
-        if keyboard_input.pressed(KeyCode::ArrowRight) {
-            direction.x += SPEED;
-        }
-        if keyboard_input.pressed(KeyCode::ArrowDown) {
-            direction.y -= SPEED;
-        }
-        if keyboard_input.pressed(KeyCode::ArrowUp) {
-            direction.y += SPEED;
-        }
-
-        if direction != Vec3::ZERO {
-            transform.translation += direction.normalize();
-        }
-
-        // direction = Vec3::ZERO;
-    }
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+    snake::spawn_snake(commands);
 }
 
 fn main() {
     App::new()
-        .add_systems(Startup, setup_camera)
-        .add_systems(Startup, spawn_snake)
-        .add_systems(Update, snake_movement)
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Snake!".to_string(),
+                    ..default()
+                }),
+                ..default()
+            })
+        )
+        .add_systems(Startup, setup)
+        .add_systems(
+            FixedUpdate,
+            (
+                snake::snake_movement,
+                arena::size_scaling,
+                arena::position_translation,
+            ).chain()
+        )
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
---
+++ src/snake.rs
@@ -0,0 +1,66 @@
+use bevy::prelude::*;
+use crate::arena;
+
+#[derive(Component)]
+pub struct SnakeHead;
+
+#[derive(Component)]
+pub struct SnakeSegment;
+
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+const SNAKE_SEGMENT_COLOR: Color = Color::rgb(0.5, 0.5, 0.5);
+const SPEED: f32 = 5.0;
+
+pub fn snake_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut head_positions: Query<&mut arena::Position, With<SnakeHead>>
+) {
+    for mut pos in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            pos.x -= 1;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            pos.x += 1;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) {
+            pos.y -= 1;
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) {
+            pos.y += 1;
+        }
+    }
+}
+
+pub fn spawn_snake(mut commands: Commands) {
+    // Add head
+    commands.spawn((
+        SnakeHead,
+        SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        },
+        arena::Position { x: 3, y: 3 },
+        arena::Size::square(0.8),
+    ));
+
+    // Add segment body
+    commands.spawn((
+        SnakeSegment,
+        SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_SEGMENT_COLOR,
+                ..default()
+            },
+            ..default()
+        },
+        arena::Position { x: 3, y: 3 },
+        arena::Size::square(0.8),
+    ));
+}
